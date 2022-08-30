7102a5e5ca68a9322794ee75b6050b3f45e8d4b5
Cecil-Anyega <ceciliaanyega@gmail.com>
2022-08-30 22:57:17 +0300

    Done

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,4 @@ require 'bundler/setup'
 Bundler.require(:default, ENV["RACK_ENV"])
 
 require_relative "../app/models/student"
+require_relative '../db/migrate/02_add_grade_and_birthdate_to_students'
